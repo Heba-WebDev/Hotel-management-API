ba5cd85cf16a0c2f4ee2c12b273043728c984221
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-18 20:09:10 +0300

    Added the routes for rooms and their controllers

--- routes/rooms.routes.js
+++ routes/rooms.routes.js
@@ -1,5 +1,20 @@
 import express from "express";
+import {
+  getAllRooms,
+  getAllRoomTypes,
+  getRoomById,
+  createRoom,
+  createRoomType,
+  updateRoom,
+  deleteRoom,
+} from "../controllers/rooms.controllers";
 
 export const roomsRouter = express.Router();
 
-roomsRouter.route("/").get(() => {});
+roomsRouter.route("/").get(getAllRooms);
+roomsRouter.route("/:id").get(getRoomById);
+roomsRouter.route("/").post(createRoom);
+roomsRouter.route("/").patch(updateRoom);
+roomsRouter.route("/").delete(deleteRoom);
+roomsRouter.route("/rooms-types").get(getAllRoomTypes);
+roomsRouter.route("/room-types").post(createRoomType);
