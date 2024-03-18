dc416c04fee504d911c32734073dbd2ef5bba43b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-18 20:07:10 +0300

    added httpStatusCode error an d fail to use instead of hard-coding it

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 import express from "express";
 import cors from "cors";
 import { roomsRouter } from "./routes/rooms.routes";
+import { ERROR, FAIL } from "./utils/httpStatusCode";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/v1", roomsRouter);
+app.use("/api/v1/rooms", roomsRouter);
 
 // Error handling middleware
 app.use((error, req, res, next) => {
-  console.error("Error:", error);
-  res.status(500).json({
-    status: "ERROR",
+  console.error("Error: ", error);
+  res.status(error?.statusCode || 500).json({
+    status: ERROR,
     message: error.message || "An error occurred.",
   });
 });
 
-// Catch-all route for unexisting endpoints
+// Catch-all unexisting route
 app.use("*", (req, res, next) => {
   res.status(404).send({
-    status: "FAIL",
+    status: FAIL,
     data: null,
     message: "Resource not found.",
   });
