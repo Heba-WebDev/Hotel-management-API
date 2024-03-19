e215549888a36ddb0cd6b293c80b8945a92bdf07
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-19 22:26:12 +0300

    Create room's schema

---
+++ models/Room.js
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+const roomSchema = new Schema({
+  name: String,
+  roomType: String,
+  price: Number,
+});
+
+const roomModel = model("rooms", roomSchema);
+
+export { roomModel };
