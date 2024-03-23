9a733fad29cc7a04e42b821ccc874de100e29dbc
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-23 20:07:33 +0300

    Added register, login and deleteAccount controllers

---
+++ controllers/users.controllers.js
@@ -0,0 +1,94 @@
+import bcrypt, { compare } from "bcrypt";
+import { generateJwt } from "../utils/generateJWT.js";
+import wrapper from "../middlewares/asyncWrapper.js";
+import { userModel } from "../models/User.js";
+import { globalError } from "../utils/globalError.js";
+import { statusCode } from "../utils/httpStatusCode.js";
+const { SUCCESS, FAIL } = statusCode;
+
+const register = wrapper(async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const userName = await userModel.findOne({ username });
+  if (userName) {
+    const err = new globalError("Username already exists.", 400, FAIL);
+    return next(err);
+  }
+  const userEmail = await userModel.findOne({ email });
+  if (userEmail) {
+    const err = new globalError("Email already exists.", 400, FAIL);
+    return next(err);
+  }
+  const hashedPass = await bcrypt.hash(password, 10);
+  const user = new userModel({
+    username,
+    email,
+    password: hashedPass,
+  });
+  await user.save();
+  res.status(201).send({
+    status: SUCCESS,
+    message: "User successfully registered.",
+    data: {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+  });
+});
+
+const login = wrapper(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    const err = new globalError("No user found.", 404, FAIL);
+    return next(err);
+  }
+  const matchedPassword = await compare(password, user.password);
+  if (!matchedPassword) {
+    const err = new globalError("Invalid Credentials.", 401, FAIL);
+    return next(err);
+  }
+  const token = await generateJwt({
+    user_id: user.id,
+    role: user.role,
+  });
+
+  res.status(200).send({
+    status: SUCCESS,
+    message: "User successfully logged in.",
+    data: {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+    token: token,
+  });
+});
+
+const deleteAccount = wrapper(async (req, res, next) => {
+  const { id } = req.body;
+  const token = req?.decodedToken;
+  const user = await userModel.findById(id);
+  if (!user) {
+    const err = new globalError("No user found.", 404, FAIL);
+    return next(err);
+  }
+  if (token?.user_id !== user.id && token?.role === "Guest") {
+    const err = new globalError(
+      "Unauthorized to perform this action.",
+      401,
+      FAIL
+    );
+    return next(err);
+  }
+
+  await user.deleteOne();
+  return res.status(204).send({
+    status: SUCCESS,
+    message: "Account sucessfully deleted.",
+  });
+});
+
+export { register, login, deleteAccount };
