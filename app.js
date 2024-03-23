925a3736c21fb3dae2ed08c99f3469948f99656d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-23 20:04:59 +0300

    Fixed import issue

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./db/mongoose.js";
 import { roomsRouter } from "./routes/rooms.routes.js";
-import { statusCode } from "./utils/httpStatusCode.js";
 import { roomTypesRouter } from "./routes/roomtypes.routes.js";
+import { userRouter } from "./routes/users.routes.js";
+import { statusCode } from "./utils/httpStatusCode.js";
 const { FAIL, ERROR } = statusCode;
 
 const app = express();
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1/users", userRouter);
 app.use("/api/v1/rooms", roomsRouter);
 app.use("/api/v1/roomsTypes", roomTypesRouter);
 
