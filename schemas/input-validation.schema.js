f9e23b3aaae39e5b2abd24c1647dba247764374f
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-23 20:13:51 +0300

    Exported an object that contains all the avaliable input validation schemas

---
+++ schemas/input-validation.schema.js
@@ -0,0 +1,15 @@
+import { registerSchema } from "./validations/auth-validation/registration.schema.js";
+import { loginSchema } from "./validations/auth-validation/login.schema.js";
+import { deleteAccountSchema } from "./validations/auth-validation/delete-account.schema.js";
+import { getRoomByIdSchema } from "./validations/rooms-validation/get-room-by-id.schema.js";
+import { createRoomSchema } from "./validations/rooms-validation/create-room.schema.js";
+
+const validationObj = {
+  registerSchema,
+  loginSchema,
+  deleteAccountSchema,
+  getRoomByIdSchema,
+  createRoomSchema,
+};
+
+export default validationObj;
